27a4da97dc10d4f39a69e2809dad59903822b548
Kaka Karsana Noerhafidan <86177774+kakakrsn@users.noreply.github.com>
2023-11-09 19:02:29 +0700

    add assets

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import IconDelete from './icon/IconDelete.svg'
+import IconDrawer from './icon/IconDrawer.svg'
+import IconEdit from './icon/IconEdit.svg'
+import IconMinus from './icon/IconMinus.svg'
+import IconPlus from './icon/IconPlus.svg'
+
+export{
+    IconDelete, IconDrawer, IconEdit, IconMinus,IconPlus, 
+}
